84ecea17fa9cdb9b3d5e771fdee00fa1c8622289
ArianneAlonso <ariannealonso8@gmail.com>
2025-03-19 23:46:25 -0300

    test si funciona el push

--- server/main.js
+++ server/main.js
@@ -1,1 +1,11 @@
-console.log("hola")
+// importacion del express
+import express from "express"
+const app = express
+//prueba de qe funciona 
+app.get("/", (req, res) => {
+    res.send("hola");
+});
+//servidor
+app.listen(400, () =>{
+    console.log("server corriendo en el puerto 4000");
+});
