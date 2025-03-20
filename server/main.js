9d2b31e788d302459d9b89d5f48feaa2f754c57c
ArianneAlonso <ariannealonso8@gmail.com>
2025-03-20 00:04:13 -0300

    se actualizo la parte del publico

--- server/main.js
+++ server/main.js
@@ -1,10 +1,16 @@
 // importacion del express
 import express from "express"
-const app = express
-//prueba de qe funciona 
+const app = express()
+
+
+//servir archivos del public
+app.use(express.static("public"));
+
+//probar que el servidor funciona
 app.get("/", (req, res) => {
-    res.send("hola");
+  res.sendFile("index.html", { root: "public" }); 
 });
+
 //servidor
 app.listen(400, () =>{
     console.log("server corriendo en el puerto 4000");
